60ab6b71c887ce0280d5a6e660b800b768cb09d0
motioncrazy <franciscojunior@me.com>
2015-05-01 00:58:16 -0300

    data structures - 1451 done

--- L3 - Data structures/1451.py
+++ L3 - Data structures/1451.py
@@ -3,25 +3,41 @@ while True:
 		line = raw_input()
 	except EOFError: break
 
-	pos = 0
-	o_pos = 0
-	output_letters = []
-	while True:
-		if pos == len(line) - 1: break
+	home_text_list = []
+	end_text = ""
+	middle_text = ""
+	current_text = ""
+	pressed_key = -1			# -1 for none, 0 for home, 1 for end
+	home_count = 0
+	for i in range(len(line)):
+		if line[i] == "[":
+			pressed_key = 0
+			home_count += 1
 
-		if line[pos] == "[":
-			o_pos = 0
-		elif line[pos] == "[":
-			o_pos = len(line) - 1
-		else:
-			o_pos = pos
+		if line[i] == "]":
+			pressed_key = 1
 
-		pos += 1
+		if line[i] != "[" and line[i] != "]":
+			if pressed_key == 1:
+				end_text += line[i]
+			elif pressed_key == 0:
+				current_text += line[i]
+				if i < len(line) - 1 and line[i + 1] == "]":
+					if home_count > 1:
+						home_text_list.insert(0, current_text)
+					else:
+						home_text_list.append(current_text)
 
-		output_letters.insert(o_pos, line[pos])
+					current_text = ""
+				
+				if i == len(line) - 1:
+					home_text_list.insert(0, current_text)
 
-	output = ""
-	for i in range(len(output_letters)):
-		output += output_letters[i]
+			else:
+				middle_text += line[i]
 
-	print output
+	home_text = ""
+	for i in range(len(home_text_list)):
+		home_text += home_text_list[i]
+
+	print home_text + middle_text + end_text
