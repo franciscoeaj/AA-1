058c90f57c0c84f84aa16bd08f05377698a98b89
motioncrazy <franciscojunior@me.com>
2015-05-01 04:30:32 -0300

    data structures - 1451 rectified

--- L3 - Data structures/1451.py
+++ L3 - Data structures/1451.py
@@ -5,39 +5,21 @@ while True:
 
 	home_text_list = []
 	end_text = ""
-	middle_text = ""
-	current_text = ""
-	pressed_key = -1			# -1 for none, 0 for home, 1 for end
-	home_count = 0
-	for i in range(len(line)):
-		if line[i] == "[":
-			pressed_key = 0
-			home_count += 1
-
-		if line[i] == "]":
-			pressed_key = 1
-
-		if line[i] != "[" and line[i] != "]":
-			if pressed_key == 1:
-				end_text += line[i]
-			elif pressed_key == 0:
-				current_text += line[i]
-				if i < len(line) - 1 and line[i + 1] == "]":
-					if home_count > 1:
-						home_text_list.insert(0, current_text)
-					else:
-						home_text_list.append(current_text)
-
-					current_text = ""
-				
-				if i == len(line) - 1:
-					home_text_list.insert(0, current_text)
-
+	home_pressed = False
+	for letter in line:
+		if letter == "[":
+			home_pressed = True
+			home_text_list.append("")
+		elif letter == "]":
+			home_pressed = False
+		else:
+			if home_pressed:
+				home_text_list[-1] += letter
 			else:
-				middle_text += line[i]
+				end_text += letter
 
 	home_text = ""
-	for i in range(len(home_text_list)):
+	for i in range(len(home_text_list) - 1, -1, -1):
 		home_text += home_text_list[i]
 
-	print home_text + middle_text + end_text
+	print home_text + end_text
